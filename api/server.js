e99831db6abfd0282361db88b51bf19a66813ff0
elizabethvilloria <lizvilloria@gmail.com>
2024-01-20 19:20:41 -0500

    Finished all tests

--- api/server.js
+++ api/server.js
@@ -6,10 +6,38 @@ const server = express()
 server.use(express.json())
 
 
-server.delete('/api/users/:id', async (req, res) => {
+server.put('/api/users/:id', async (req, res) => {
     try {
         const possibleUser = await User.findById(req.params.id)
-    console.log('possible user ', possibleUser)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" 
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                })
+            } else {
+                const updatedUser = await User.update(
+                    req.params.id, 
+                    req.body
+                    )
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating user',
+            err: err.message, 
+            stack: err.stack
+        })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+    const possibleUser = await User.findById(req.params.id)
     if(!possibleUser) {
         res.status(404).json({
             message: "The user with the specified ID does not exist" 
@@ -20,12 +48,14 @@ server.delete('/api/users/:id', async (req, res) => {
     }
     } catch (err) {
         res.status(500).json({
-            message: 'error creating user',
+            message: 'error deleting user',
             err: err.message, 
             stack: err.stack
         })
     }
 })
+
+
 
 server.post('/api/users', (req, res) => {
     const user = req.body;
